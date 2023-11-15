bdc31b12e7c08291a7c939e282e92fcbc23ac138
Khoi1201 <Acknowledge999@gmail.com>
2023-11-15 12:03:44 +0700

    feat: authen route

--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
-const env = require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const axios = require("axios");
+const env = require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const axios = require('axios')
 
-const authRouter = require("./routes/auth");
-// const productRouter = require("./routes/product");
+const authRouter = require('./routes/auth')
+// const userRouter = require('./routes/user')
+const productRouter = require("./routes/product");
 
 const connectDB = async () => {
-    try {
-      mongoose.connect(process.env.DB_CONNECT);
-      console.log("MongoDB connected");
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
+  try {
+    mongoose.connect(process.env.DB_CONNECT)
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
 
-  connectDB();
-const app = express();
-app.use(express.json());
+connectDB()
+const app = express()
+app.use(express.json())
 app.use(cors())
 
-app.use("/api/auth", authRouter, (req, res) => {});
-// app.use("/api/posts", productRouter);
+app.use('/api/auth', authRouter, (req, res) => {})
+// app.use('/api/user', userRouter)
+app.use("/api/products", productRouter);
 
-const PORT = process.env.PORT || 1203;
+const PORT = process.env.PORT || 1203
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+  console.log(`Server started on port ${PORT}`)
+})
