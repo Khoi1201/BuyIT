dcce14b01e09949bfeba6aa40478b2384fc942bd
Khoi1201 <Acknowledge999@gmail.com>
2024-04-10 13:11:53 +0700

    fix: vercel deploy

--- client/src/redux/api/axios.custom.js
+++ client/src/redux/api/axios.custom.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export const axiosCustom = axios.create({
   baseURL:
-    process.env.NODE_ENV !== 'production'
+    process.env.NODE_ENV === 'production'
       ? process.env.REACT_APP_API_HOST
       : 'https://buy-it-ten.vercel.app/',
   headers: {
